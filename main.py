966897413a97050cfae2b13af6d8c50b918e89a0
Mario Mori <mario_mori@hotmail.com>
2023-10-18 19:21:49 -0300

    Almoust finish the main bot

---
+++ main.py
@@ -0,0 +1,124 @@
+from discord.ext import tasks, commands
+from discord import app_commands
+from models import Currency
+from dotenv import read_dotenv
+from datetime import datetime, time
+from pytz import timezone
+
+import discord
+import os
+import aiohttp
+import requests
+
+def get_datetime() -> str:
+    n = datetime.now(timezone('America/Argentina/Buenos_Aires'))
+    return n.strftime('%Y-%m-%dT%H:%M:%S.%f%:z')
+
+def check_dates(internal_time: str, external_time: str) -> bool:
+    time_format = "%Y-%m-%dT%H:%M:%S.%f%z"
+    internal = datetime.strptime(internal_time, time_format)
+    external = datetime.strptime(external_time, time_format)
+    if internal < external:
+        return True
+    else:
+        return False
+
+# Quotes
+DOLLAR_BLUE = Currency("Dolar blue")
+DOLLAR = Currency("Dolar oficial")
+EURO = Currency("Euro oficial")
+EURO_BLUE = Currency("Euro blue")
+
+def set_currency() -> str:
+    response = requests.get("https://api.bluelytics.com.ar/v2/latest")
+    res = response.json()
+    DOLLAR.update(res["oficial"])
+    DOLLAR_BLUE.update(res["blue"])
+    EURO.update(res["oficial_euro"])
+    EURO_BLUE.update(res["blue_euro"])
+    return res["last_update"]
+
+# Server
+GUILD = discord.Object(id=1161842553352630313)
+TESTING_CHANNEL = 1163914190159880304
+TIMES = [
+            time(hour=10, tzinfo=timezone('America/Argentina/Buenos_Aires')),
+            time(hour=19, minute=19, tzinfo=timezone('America/Argentina/Buenos_Aires'))
+    ]
+class Sora(discord.Client):
+
+    # TODO task with the inform of opening and closing of market
+    def __init__(self, last_update: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.tree = app_commands.CommandTree(self)
+        self._last_update =  last_update
+
+    async def setup_hook(self) -> None:
+        # start the task to run in the background
+        self.bluelytics.start()
+        self.market.start()
+        self.tree.copy_global_to(guild=GUILD)
+        await self.tree.sync(guild=GUILD)
+
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+    @tasks.loop(minutes=30.0)  # task runs every 60 seconds
+    async def bluelytics(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://api.bluelytics.com.ar/v2/latest") as res:
+                data = await res.json()
+                if check_dates(self._last_update, data["last_update"]):
+                    DOLLAR.update(data["oficial"])
+                    DOLLAR_BLUE.update(data["blue"])
+                    EURO.update(data["oficial_euro"])
+                    EURO_BLUE.update(data["blue_euro"])
+                    self._last_update = data["last_update"]
+    
+    @tasks.loop(time=TIMES)
+    async def market(self):
+        channel = self.get_channel(TESTING_CHANNEL)
+        t = datetime.now()
+        print("INGRESE AQUI")
+        if t.hour == 10:
+            await self.bluelytics()
+            message = f"""Holis!!!! arrancamos el mercado con el dolar blue en los valores:
+            {DOLLAR_BLUE.__str__()}"""
+            channel.send(message)
+        if t.hour == 19:
+            await self.bluelytics()
+            message = f"""Holis!!!! finaliza el mercado con el dolar blue en los valores:
+            {DOLLAR_BLUE.__str__()}"""
+            channel.send(message)
+
+
+
+    @bluelytics.before_loop
+    async def before_bluelytics(self):
+        await self.wait_until_ready()  # wait until the bot logs in
+    
+    @market.before_loop
+    async def before_market(self):
+        await self.wait_until_ready()
+
+
+def main():
+    print(TIMES)
+    read_dotenv()
+    TOKEN = os.getenv('DISCORD_TOKEN')
+    client = Sora(last_update=set_currency(), intents=discord.Intents.default())
+    @client.tree.command(name="dolar_blue")
+    async def dollar_blue(interaction: discord.Integration):
+        await interaction.response.send_message(f"{DOLLAR_BLUE.__str__()}", ephemeral=True)
+
+    @client.tree.command(name="dolares_blue_a_pesos")
+    @app_commands.describe(dolares = "Por cuantos dolares se debe multiplicar el dolar de mercado (blue)")
+    async def dollar_blue_to_pesos(interaction: discord.Interaction, dolares: float):
+        pesos = dolares * DOLLAR_BLUE.sell
+        await interaction.response.send_message(f"Son {pesos} pesos", ephemeral=True)
+    
+    client.run(TOKEN)
+
+if __name__ == "__main__":
+    main()
--- models.py
+++ models.py
@@ -2,14 +2,13 @@ import json
 
 class Currency:
 
-    def __init__(self, name: str, buy: float, avg: float, sell: float):
+    def __init__(self, name: str):
         self._name = name
-        self._buy = buy
-        self._avg = avg
-        self._sell = sell
+        self._buy: float
+        self._sell: float
 
     def __str__(self):
-        return f"{self.name}: {self.avg}"
+        return f"Compra: {self.buy}\n Venta: {self.sell}"
     
     def json(self):
         """
@@ -17,39 +16,20 @@ class Currency:
         """
         json_object = {
             "compra": self._buy,
-            "promedio": self._avg,
             "venta": self._sell
         }
         return json.dumps(json_object)
     
+    def update(self, object: dict) -> None:
+        self._buy = object["value_buy"]
+        self._sell = object["value_sell"]
+
     @property
     def name(self):
         return self._name
     @property
     def buy(self):
         return self._buy
-    
-    @property
-    def avg(self):
-        return self._avg
-    
-    @property
+    @property 
     def sell(self):
         return self._sell
-    
-    @buy.setter
-    def buy(self, buy):
-        if self._buy != buy:
-            self._buy = buy
-
-    @avg.setter
-    def avg(self, avg):
-        if self._avg != avg:
-            self._avg = avg
-    
-    @sell.setter
-    def sell(self, sell):
-        if self._sell != sell:
-            self._sell = sell
-
-
